913458fc718a87be963f3b839f812ebb50c3733f
MacbookAirM1 <macbookairm1@MacbookAirM1s-MacBook-Air.local>
2026-02-06 16:08:37 -0700

    gin server listening on all network interfaces added

---
+++ server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func ServeGin() {
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "0.0.0.0" // listen on all network interfaces
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	gin.SetMode(gin.ReleaseMode) // remove distracting logging
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") || strings.HasPrefix(origin, "http://127.0.0.1") {
+				return true
+			}
+			return false
+		},
+		AllowMethods:  []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+		MaxAge:        12 * time.Hour,
+	}))
+
+	r.GET("/ws", func(c *gin.Context) {
+		// upgrader here
+	})
+
+	log.Printf("Serving Gin at ")
+	svr := fmt.Sprintf("%s:%s", host, port)
+	r.Run(svr)
+}
